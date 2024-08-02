50883a4aa715dd7d20b5ec14113010014de97c8d
pocco <ssilver121@naver.com>
2024-08-02 16:43:15 +0900

    Edit 페이지 UI 및 기능 구현
    https://www.inflearn.com/course/lecture?courseSlug=%ED%95%9C%EC%9E%85-%EB%A6%AC%EC%95%A1%ED%8A%B8&unitId=210900&tab=curriculum

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,33 +1,12 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-const emotionList = [
-    {
-        emotionId: 1,
-        emotionName: "완전 좋음",
-    },
-    {
-        emotionId: 2,
-        emotionName: "좋음",
-    },
-    {
-        emotionId: 3,
-        emotionName: "그럭저럭",
-    },
-    {
-        emotionId: 4,
-        emotionName: "나쁨",
-    },
-    {
-        emotionId: 5,
-        emotionName: "끔찍함",
-    },
-];
+import { emotionList } from "../util/constants";
 
 const getStringedDate = (targetDate) => {
+    // yyyy-mm-dd
     let year = targetDate.getFullYear();
     let month = targetDate.getMonth() + 1;
     let date = targetDate.getDate();
@@ -42,13 +21,23 @@ const getStringedDate = (targetDate) => {
     return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
     const [input, setInput] = useState({
         createdDate: new Date(),
         emotionId: 3,
         content: "",
     });
+
     const nav = useNavigate();
+
+    useEffect(() => {
+        if (initData) {
+            setInput({
+                ...initData,
+                createdDate: new Date(Number(initData.createdDate)),
+            });
+        }
+    }, [initData]);
 
     const onChangeInput = (e) => {
         let name = e.target.name;
@@ -103,6 +92,7 @@ const Editor = ({ onSubmit }) => {
                 <h4>오늘의 일기</h4>
                 <textarea
                     name="content"
+                    value={input.content}
                     onChange={onChangeInput}
                     placeholder="오늘은 어땠나요?"
                 />

6456128d7f9cc18b3d5aa8046ba44731de021b5a
pocco <ssilver121@naver.com>
2024-08-02 13:56:06 +0900

    일기 관리 기능 구현하기 1
    https://www.inflearn.com/course/lecture?courseSlug=%ED%95%9C%EC%9E%85-%EB%A6%AC%EC%95%A1%ED%8A%B8&unitId=103545&tab=curriculum

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
 import './App.css'
 
+import {useReducer} from "react";
+
 import {Routes, Route, Link, useNavigate} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
+import Edit from "./pages/Edit.jsx";
 import Notfound from "./pages/Notfound.jsx";
-import Button from "./components/Button.jsx";
-import Header from "./components/Header.jsx";
 
-import {getEmotionImage} from "./util/get-emotion-image.js";
+const mockData = [
+    {
+        id: 1,
+        createdDate: new Date().getTime(),
+        emotionId: 1,
+        content: "1번 일기 내용입니다."
+    },
+    {
+        id: 2,
+        createdDate: new Date().getTime(),
+        emotionId: 2,
+        content: "2번 일기 내용입니다."
+    },
+];
+
+function reducer(state, action) {
+    return state;
+}
+
 
 function App() {
-    const nav = useNavigate();
+    const [data, dispatch] = useReducer(reducer, []);
 
-    const onClickButton = () => {
-        nav('/new');
-    }
     return (
         <>
-            <Header
-                title={"Header"}
-                leftChild={<Button text={"Left"} />}
-                rightChild={<Button text={"right"} />}
-            />
-            <Button
-                text={"123"}
-                onClick={() => {
-                    console.log("버튼이 클릭되었습니다.");
-                }}
-                type={"POSITIVE"}
-            />
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/new" element={<New/>}/>
                 <Route path="/diary/:id" element={<Diary/>}/>
+                <Route path="/edit/:id" element={<Edit/>}/>
                 <Route path="*" element={<Notfound/>}/>
             </Routes>
         </>
